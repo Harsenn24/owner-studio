c835201be70f5244fa963dedbd2fa70d48bbb8ce
Harsenn <harsenn@aptika.co.id>
2025-11-07 15:41:50 +0700

    form submission oke

--- src/components/HomePage/HomePage.vue
+++ src/components/HomePage/HomePage.vue
@@ -126,10 +126,15 @@
               <!-- Nama Studio -->
               <div>
                 <label class="text-xs font-medium text-slate-600">Nama Studio</label>
-                <input v-model="form.name" required
+                <input v-model="form.name" @input="handleNameInput" required minlength="5" maxlength="20"
                   class="mt-2 w-full rounded-lg border px-3 py-2 text-sm border-black text-black"
                   placeholder="Contoh: Studio Suara Jakarta" />
+                <p v-if="form.name.length > 0 && (form.name.length < 5 || form.name.length > 20)"
+                  class="text-xs text-red-500 mt-1">
+                  Nama studio harus antara 5–20 karakter.
+                </p>
               </div>
+
               <!-- Provinsi -->
               <div class="relative" ref="provinceContainer">
                 <label class="text-xs font-medium text-slate-600">Provinsi</label>
@@ -203,69 +208,142 @@
               <!-- Google Maps -->
               <div>
                 <label class="text-xs font-medium text-slate-600">Tautan Google Maps</label>
-                <input v-model="form.gmaps"
+                <input v-model="form.gmaps" @input="handleGmapsInput" required
                   class="mt-2 w-full rounded-lg border px-3 py-2 text-sm border-black text-black"
                   placeholder="https://maps.app.goo.gl/..." />
+                <p v-if="form.gmaps && form.gmaps.length < 5" class="text-xs text-red-500 mt-1">
+                  Tautan minimal 5 karakter.
+                </p>
+                <p v-else-if="form.gmaps && !isValidUrl(form.gmaps)" class="text-xs text-red-500 mt-1">
+                  Harus berupa tautan (URL) yang valid.
+                </p>
               </div>
+
             </div>
 
             <!-- Alamat Lengkap -->
             <div>
               <label class="text-xs font-medium text-slate-600">Alamat Lengkap</label>
-              <input v-model="form.address"
-                class="mt-2 w-full rounded-lg border px-3 py-5 text-sm border-black text-black"
-                placeholder="Jalan Sukmajaya 1 NO ...." />
+              <textarea v-model="form.address" @input="handleAddressInput" required rows="3"
+                class="mt-2 w-full rounded-lg border px-3 py-2 text-sm border-black text-black resize-none overflow-hidden"
+                placeholder="Contoh: Jalan Sukmajaya 1 No. 23, Depok"></textarea>
+
+              <p v-if="form.address && form.address.length < 10" class="text-xs text-red-500 mt-1">
+                Alamat minimal 10 karakter.
+              </p>
             </div>
+
 
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
               <!-- Contact Person Name -->
               <div>
                 <label class="text-xs font-medium text-slate-600">Contact Person Name</label>
-                <input v-model="form.contactName" required
+                <input v-model="form.contactName" @input="handleContactNameInput" required minlength="5" maxlength="20"
                   class="mt-2 w-full rounded-lg border px-3 py-2 text-sm border-black text-black"
                   placeholder="Contoh: Dhimas" />
+                <p v-if="form.contactName.length > 0 && (form.contactName.length < 5 || form.contactName.length > 20)"
+                  class="text-xs text-red-500 mt-1">
+                  Nama contact person harus antara 5–20 karakter.
+                </p>
               </div>
               <!-- Contact Person Phone -->
               <div>
                 <label class="text-xs font-medium text-slate-600">Contact Person Phone</label>
-                <input v-model="form.contactPhone" required
+                <input v-model="form.contactPhone" @input="handlePhoneInput" required maxlength="15"
                   class="mt-2 w-full rounded-lg border px-3 py-2 text-sm border-black text-black"
                   placeholder="Contoh: 08135115415" />
+
+                <p v-if="phoneError" class="text-xs text-red-500 mt-1">
+                  {{ phoneError }}
+                </p>
               </div>
+
               <!-- Bank -->
-              <div>
+              <div class="relative" ref="provinceContainer">
                 <label class="text-xs font-medium text-slate-600">Bank</label>
-                <input v-model="form.bank" required
-                  class="mt-2 w-full rounded-lg border px-3 py-2 text-sm border-black text-black"
-                  placeholder="Contoh: BCA" />
+                <input type="text" v-model="bankSearch" @input="fetchBank" @focus="fetchBank" placeholder="nama bank..."
+                  class="mt-2 w-full rounded-lg border px-3 py-2 text-sm border-black text-black!" />
+                <ul v-if="bankList.length"
+                  class="absolute w-full max-h-48 overflow-auto border rounded mt-1 bg-white z-50 text-black! py-2">
+                  <li v-for="bank in bankList" :key="bank.id" @click="selectBank(bank)"
+                    class="px-3 py-2 hover:bg-gray-100 cursor-pointer">
+                    {{ bank.name }}
+                  </li>
+                </ul>
               </div>
               <!-- Nomor Rekening Bank -->
               <div>
                 <label class="text-xs font-medium text-slate-600">Nomor Rekening Bank</label>
-                <input v-model="form.bankAccount" required
+                <input v-model="form.bankAccount" @input="handleBankAccountInput" required minlength="5" maxlength="20"
                   class="mt-2 w-full rounded-lg border px-3 py-2 text-sm border-black text-black"
                   placeholder="Contoh: 5544512536" />
+                <p v-if="form.bankAccount && form.bankAccount.length < 5" class="text-xs text-red-500 mt-1">
+                  Nomor Rekening minimal 5 karakter.
+                </p>
               </div>
+
             </div>
 
             <!-- Upload Images -->
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-6 mt-4">
+              <!-- Upload KTP -->
               <div>
-                <label class="text-xs font-medium text-slate-600">Upload KTP</label>
-                <input type="file" accept="image/*" @change="handleFileUpload($event, 'ktp')"
-                  class="mt-2 w-full rounded-lg border px-3 py-2 text-sm border-black text-black" />
+                <label class="text-sm font-medium text-slate-700">Upload KTP</label>
+
+                <input v-if="!previewFile.ktp" type="file" accept="image/*" required
+                  @change="handleFileUpload($event, 'ktp')"
+                  class="mt-2 w-full rounded-lg border px-3 py-3 text-sm border-black text-black cursor-pointer" />
+
+                <div v-else class="mt-3 relative rounded-lg overflow-hidden border border-gray-300 shadow-md">
+                  <img :src="previewFile.ktp" alt="Preview KTP" class="w-full aspect-[16/9] object-cover" />
+                  <button @click="removeFile('ktp')"
+                    class="absolute top-2 right-2 bg-white bg-opacity-80 hover:bg-opacity-100 text-xs font-medium px-2 py-1 rounded shadow">
+                    Ganti Foto
+                  </button>
+                </div>
+
+                <p v-if="errorsFile.ktp" class="text-xs text-red-500 mt-1">{{ errorsFile.ktp }}</p>
               </div>
+
+              <!-- Upload Studio 1 -->
               <div>
-                <label class="text-xs font-medium text-slate-600">Upload Foto Studio 1</label>
-                <input type="file" accept="image/*" @change="handleFileUpload($event, 'studio1')"
-                  class="mt-2 w-full rounded-lg border px-3 py-2 text-sm border-black text-black" />
+                <label class="text-sm font-medium text-slate-700">Upload Foto Studio 1</label>
+
+                <input v-if="!previewFile.studio1" type="file" accept="image/*" required
+                  @change="handleFileUpload($event, 'studio1')"
+                  class="mt-2 w-full rounded-lg border px-3 py-3 text-sm border-black text-black cursor-pointer" />
+
+                <div v-else class="mt-3 relative rounded-lg overflow-hidden border border-gray-300 shadow-md">
+                  <img :src="previewFile.studio1" alt="Preview Studio 1" class="w-full aspect-[16/9] object-cover" />
+                  <button @click="removeFile('studio1')"
+                    class="absolute top-2 right-2 bg-white bg-opacity-80 hover:bg-opacity-100 text-xs font-medium px-2 py-1 rounded shadow">
+                    Ganti Foto
+                  </button>
+                </div>
+
+                <p v-if="errorsFile.studio1" class="text-xs text-red-500 mt-1">{{ errorsFile.studio1 }}</p>
               </div>
+
+              <!-- Upload Studio 2 -->
               <div>
-                <label class="text-xs font-medium text-slate-600">Upload Foto Studio 2</label>
-                <input type="file" accept="image/*" @change="handleFileUpload($event, 'studio2')"
-                  class="mt-2 w-full rounded-lg border px-3 py-2 text-sm border-black text-black" />
+                <label class="text-sm font-medium text-slate-700">Upload Foto Studio 2</label>
+
+                <input v-if="!previewFile.studio2" type="file" accept="image/*" required
+                  @change="handleFileUpload($event, 'studio2')"
+                  class="mt-2 w-full rounded-lg border px-3 py-3 text-sm border-black text-black cursor-pointer" />
+
+                <div v-else class="mt-3 relative rounded-lg overflow-hidden border border-gray-300 shadow-md">
+                  <img :src="previewFile.studio2" alt="Preview Studio 2" class="w-full aspect-[16/9] object-cover" />
+                  <button @click="removeFile('studio2')"
+                    class="absolute top-2 right-2 bg-white bg-opacity-80 hover:bg-opacity-100 text-xs font-medium px-2 py-1 rounded shadow">
+                    Ganti Foto
+                  </button>
+                </div>
+
+                <p v-if="errorsFile.studio2" class="text-xs text-red-500 mt-1">{{ errorsFile.studio2 }}</p>
               </div>
             </div>
+
 
             <!-- Buttons -->
             <div class="md:col-span-2 flex justify-end gap-3 mt-4">
@@ -320,6 +398,14 @@ const postalCodeSearch = ref('')
 const postalCodeList = ref([])
 const postalCodeContainer = ref(null)
 
+const bankSearch = ref('')
+const bankList = ref([])
+
+const phoneError = ref('')
+
+const previewFile = ref({ ktp: null, studio1: null, studio2: null })
+const errorsFile = ref({ ktp: '', studio1: '', studio2: '' })
+
 const form = reactive({
   name: '',
   city: '',
@@ -327,7 +413,135 @@ const form = reactive({
   gmaps: '',
   province: '',
   postalCode: '',
+  bank: '',
+  bankAccount: '',
+  contactName: '',
+  contactPhone: '',
+  document_ids : []
 })
+
+function removeFile(type) {
+  previewFile.value[type] = null
+  errorsFile.value[type] = ''
+}
+
+async function handleFileUpload(event, type) {
+  const file = event.target.files[0]
+  if (!file) return
+
+  errorsFile.value[type] = ''
+
+  // Validasi ukuran file (maksimal 500 KB)
+  if (file.size > 500 * 1024) {
+    errorsFile.value[type] = 'Ukuran file maksimal 500 KB.'
+    return
+  }
+
+  // Preview langsung (tanpa validasi rasio)
+  const fileUrl = URL.createObjectURL(file)
+  previewFile.value[type] = fileUrl
+
+  // Tentukan module name
+  const moduleName = type === 'ktp' ? 'studio.ktp' : 'studio.photo'
+
+  try {
+    const BE_BASE_URL = import.meta.env.VITE_STUDIO_BAND_BE_BASE_URL
+    const token = localStorage.getItem('token')
+    const deviceId = localStorage.getItem('device_id')
+    const ip = await getIpAdresses()
+
+    const formData = new FormData()
+    formData.append('image', file)
+    formData.append('module', moduleName)
+
+    const response = await axios.post(`${BE_BASE_URL}owner/file/upload`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'authorization': `Bearer ${token}`,
+        'x-device-id': deviceId,
+        'x-ip-address': ip,
+        'x-request-id': uuidv4()
+      }
+    })
+
+    if (!response.data.status) {
+      errorsFile.value[type] = 'Upload gagal. Silakan coba lagi.'
+    } else {
+      console.log(`✅ ${type} berhasil diupload:`, response.data.data)
+      form.document_ids.push(response.data.data.file_id)
+    }
+  } catch (err) {
+    console.error(err)
+    errorsFile.value[type] = 'Terjadi kesalahan saat upload.'
+  }
+}
+
+function handleNameInput(e) {
+  let value = e.target.value
+
+  value = value
+    .toLowerCase()
+    .replace(/\b\w/g, char => char.toUpperCase())
+
+  form.name = value
+}
+
+function handleContactNameInput(e) {
+  let value = e.target.value
+
+  value = value
+    .toLowerCase()
+    .replace(/\b\w/g, char => char.toUpperCase())
+
+  form.contactName = value
+}
+
+function handleGmapsInput() {
+  form.gmaps = form.gmaps.trim()
+}
+
+function isValidUrl(value) {
+  try {
+    new URL(value)
+    return true
+  } catch (_) {
+    return false
+  }
+}
+
+function handleBankAccountInput() {
+  form.bankAccount = form.bankAccount.replace(/\D/g, '')
+}
+
+
+function handlePhoneInput() {
+  const value = form.contactPhone
+
+  form.contactPhone = value.replace(/\D/g, '')
+
+  if (!form.contactPhone.startsWith('08')) {
+    phoneError.value = 'Nomor harus diawali dengan 08.'
+  } else if (form.contactPhone.length < 10) {
+    phoneError.value = 'Nomor minimal 10 digit.'
+  } else if (form.contactPhone.length > 15) {
+    phoneError.value = 'Nomor maksimal 15 digit.'
+  } else {
+    phoneError.value = ''
+  }
+}
+
+function handleAddressInput(e) {
+  const textarea = e.target
+
+  textarea.style.height = 'auto'
+  textarea.style.height = textarea.scrollHeight + 'px'
+
+  if (form.address && form.address.length < 10) {
+    console.warn('Alamat terlalu pendek')
+  }
+}
+
+
 
 function logout() {
   localStorage.clear()
@@ -515,6 +729,9 @@ async function fetchPostalCode(province_id = form.province, city_id = form.city,
       method: 'POST',
       url: `${BE_BASE_URL}owner/address/postal-code-pagination`,
       data: {
+        page: 1,
+        limit: 10000,
+        search: postalCodeSearch.value,
         province_id,
         city_id,
         district_id,
@@ -535,6 +752,35 @@ function selectPostalCode(postalCode) {
   form.postalCode = postalCode.id
   postalCodeSearch.value = postalCode.postal_code
   postalCodeList.value = []
+}
+
+
+async function fetchBank() {
+  try {
+    const BE_BASE_URL = import.meta.env.VITE_STUDIO_BAND_BE_BASE_URL
+    const response = await axios({
+      method: 'POST',
+      url: `${BE_BASE_URL}owner/funding/bank-list`,
+      data: {
+        page: 1,
+        limit: 10000,
+        search: bankSearch.value
+      }
+    })
+
+    if (response.data.status) {
+      bankList.value = response.data.data.data
+    }
+  } catch (error) {
+    console.error(error)
+    alert('Gagal memuat data bank. Silakan coba lagi.')
+  }
+}
+
+function selectBank(bank) {
+  form.bank = bank.id
+  bankSearch.value = bank.name
+  bankList.value = []
 }
 
 function changePage(p) {
